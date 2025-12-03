f196f0c0cd15cdadeb763411c64453613bf430dc
Iris D <iris.dijkstra@hotmail.com>
2025-12-03 15:25:32 +0100

    basic setup react project

---
+++ code/05 Essentials Practice/01-starting-project/src/components/ResultsTable.jsx
@@ -0,0 +1,4 @@
+export default function ResultsTable () {
+  return
+  <table></table>
+}
---
+++ code/05 Essentials Practice/01-starting-project/src/components/UserInput.jsx
@@ -0,0 +1,13 @@
+export default function UserInput() {
+  return (
+  <div className="">
+    <section>
+      <p>Hoi</p>
+    </section>
+  </div>
+  )
+
+
+  // Initial Investment - Annual Investment
+  // Expected Return - Duration
+}
